e5726220a66e74215092c7685fb545f1c2b0f7f6
val <dvmarinoff@gmail.com>
2014-05-16 12:21:50 +0300

    js templete added

--- js/project-1.js
+++ js/project-1.js
@@ -1,6 +1,35 @@
-(function() {
+// Example templete 1 - simple:
+(function () {
     'use strict';
     
+    var printResult;
     
+    function solution() {
+        var result;
+        
+        // solution algorithm:
+        
+        return result;
+    }
     
-} ());
+    // get input, call solution function, print result
+    printResult = function () {
+        var input, result;
+        
+        // get input
+        input = document.getElementById('input-1-1').value;
+        
+        // optional: validate input
+        //input = parseInt(input); ...    
+        
+        // call solution function: 
+        
+        // print result
+        result = input;
+        document.getElementById('result-1').innerHTML = result;
+    };
+    
+    // Event listeners:
+    document.getElementById('btn-1-submit').addEventListener('click', printResult);
+    
+}());
